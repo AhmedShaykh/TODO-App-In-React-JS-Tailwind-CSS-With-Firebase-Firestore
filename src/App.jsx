8dd5ab2baf7eac7a41f5606c3709bc38ae98837c
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-03 13:21:50 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
 import './App.css';
 
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#1D96A1] to-[#12EBCE]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  // count: `text-center p-2`,
+};
+
+
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  // const [input, setInput] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TODO App In React.JS With Firebase!
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>TODO APP</h3>
+        <form className={style.form}>
+          <input
+            // value={input}
+            // onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type='text'
+            placeholder='Add Todo'
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+
+        </ul>
+      </div>
     </div>
   );
 }
