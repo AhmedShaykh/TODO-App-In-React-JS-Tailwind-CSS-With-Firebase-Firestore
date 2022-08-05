8d42427674df17f824d05f700e2321a142f6231b
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-06 01:06:57 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ import {
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#1D96A1] to-[#12EBCE]`,
-  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
-  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  container: `bg-slate-800 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-100 p-2`,
   form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count: `text-center p-2`,
+  input: `border p-2 w-full text-xl bg-slate-400 placeholder-white text-gray-900`,
+  button: `border p-4 ml-2 bg-slate-200`,
+  count: `text-center p-2 text-gray-100 font-bold`,
 };
 
 
