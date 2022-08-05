42ae3395a334aadc02acb09c792967802ebbf708
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-06 01:07:32 +0500

    It's Time To Push !

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaRegTrashAlt } from 'react-icons/fa';
 
 const style = {
-    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    li: `flex justify-between bg-slate-600 text-slate-100 p-4 my-2 capitalize`,
     liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
     row: `flex`,
     text: `ml-2 cursor-pointer`,
@@ -12,10 +12,12 @@ const style = {
 
 const Todo = ({ todo }) => {
     return (
-        <li className={style.li}>
+        <li className={todo.completed ? style.liComplete : style.li}>
             <div className={style.row}>
-                <input type='checkbox' />
-                <p className={style.text}>{todo.text}</p>
+                <input type='checkbox' checked={todo.completed ? 'checked' : ''} />
+                <p className={todo.completed ? style.textComplete : style.text}>
+                    {todo.text}
+                </p>
             </div>
             <button>{<FaRegTrashAlt />}</button>
         </li>
